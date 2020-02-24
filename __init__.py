fe86ebd7e3e0f24f6e6129095f19b4b553c75b96
Gustavo Warmling Teixeira <gustavo@trustedhealth.com>
2020-02-24 17:38:58 -0300

    Add option to actor behave like a relayBoard

--- __init__.py
+++ __init__.py
@@ -21,48 +21,50 @@ class TimedAgitator(ActorBase):
     gpio = Property.Select("GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27], description="GPIO to which the actor is connected")
     ta_start = Property.Number("Running Timer", configurable=True, description="Defines how long the Agitator should run before pause.")
     ta_stop  = Property.Number("Pause Timer", configurable=True, description="Defines how long the Agitator should stop before run again.")
+    is_relayboard  = Property.Boolean("Act as Relayboard", configurable=True, description="Check this if you are using a Relayboard logic")
     started = 0
-    
+    gpio_on = 0 if is_relayboard else 1
+    gpio_off = 1 if is_relayboard else 0
+
     def init(self):
         GPIO.setup(int(self.gpio), GPIO.OUT)
-        GPIO.output(int(self.gpio), 0)
+        GPIO.output(int(self.gpio), gpio_off)
 
     def on(self, power=0):
         print ("GPIO ON %s" % str(self.gpio))
-        GPIO.output(int(self.gpio), 1)
+        GPIO.output(int(self.gpio), gpio_on)
 
 
     def off(self):
         print ("GPIO OFF")
-        GPIO.output(int(self.gpio), 0)
-        
-   
+        GPIO.output(int(self.gpio), gpio_off)
+
 
     def timedAggitator (self):
          while (self.started == 1):
             self.api.cache.get("actors").get(int(self.id)).timer =  int(time.time()) + int(self.ta_start) -1
             self.api.switch_actor_on(self.id)
             self.sleep(int(self.ta_start))
-            
+
             self.api.cache.get("actors").get(int(self.id)).timer = int(time.time()) + int(self.ta_stop) -1
             self.api.switch_actor_off(self.id)
             self.sleep(int(self.ta_stop))
-                    
+
          else:
             self.api.cache.get("actors").get(int(self.id)).timer =  None
             self.api.switch_actor_on(self.id)
             self.api.switch_actor_off(self.id)
 
-    
-    @cbpi.action("Start Agitator Timer")        
+
+    @cbpi.action("Start Agitator Timer")
     def start(self):
         self.started = 1
         self.api.notify(headline="Timed Agitator", message="Timed Agitator started", type="info")
         self.timedAggitator()
-        
-        
-    @cbpi.action("Stop Agitator Timer")        
+
+
+    @cbpi.action("Stop Agitator Timer")
     def stop(self):
         self.api.notify(headline="Timed Agitator", message="Timed Agitator will be stopped after one cycle.", type="info")
         self.started = 0
-    
+
